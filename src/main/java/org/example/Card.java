68080ff2fc97485549bff3f09655444965e64281
yurididomenico <yurididomenico93@gmail.com>
2024-10-03 14:36:21 +0200

    Card

---
+++ src/main/java/org/example/Card.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Card {
+    private String value;
+    private boolean isMatched;
+
+    public Card(String value) {
+        this.value = value;
+        this.isMatched = false;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
+
+    @Override
+    public String toString() {
+        return isMatched ? value : "*";
+    }
+}
