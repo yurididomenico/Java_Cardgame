87f7bab1c314a7de78bc81dc514228a96d5d7b96
yurididomenico <yurididomenico93@gmail.com>
2024-10-03 11:10:07 +0200

    reset develop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Deck deck = new Deck();
-        Player player1 = new Player("Yuri");
-        Player player2 = new Player("Ludovica");
-
-        for (int i = 0; i < 5; i++) {
-            player1.drawCard(deck);
-            player2.drawCard(deck);
-        }
-
-        player1.showHand();
-        System.out.println("-----");
-        player2.showHand();
+        System.out.println("YDD Games");
     }
 }
