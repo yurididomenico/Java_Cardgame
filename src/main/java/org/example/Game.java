0162b22edce356046a90e5deac198549f5781fa8
yurididomenico <yurididomenico93@gmail.com>
2024-10-03 18:12:43 +0200

    fx eccezione non int

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Game {
@@ -8,7 +9,7 @@ public class Game {
     private boolean gameEnded;
 
     public Game() {
-        String[] values = {"A", "B", "C", "D", "E", "F", "G", "H"};
+        String[] values = {"A", "B", "C", "D"};
         deck = new Deck(values);
         gameEnded = false;
     }
@@ -19,36 +20,61 @@ public class Game {
         while (!gameEnded) {
 
             printCards();
-            System.out.print("Seleziona la prima carta (0, " + (deck.getSize() - 1) + "): ");
-            int firstIndex = scanner.nextInt();
-            System.out.print("Seleziona la seconda carta (0, " + (deck.getSize() - 1) + "): ");
-            int secondIndex = scanner.nextInt();
 
-            if (firstIndex >= 0 &&
-                    firstIndex < deck.getSize() &&
-                    secondIndex >= 0 &&
-                    secondIndex < deck.getSize() &&
-                    firstIndex != secondIndex) {
+            int firstIndex = -1;
+            int secondIndex = -1;
+
+            while (true) {
+                try {
+                    System.out.print("Seleziona la prima carta (0, " + (deck.getSize() - 1) + "): ");
+                    firstIndex = scanner.nextInt();
+                    break; // Esce dal ciclo se l'input è valido
+                } catch (InputMismatchException e) {
+                    System.out.println("Input non valido! Inserisci un numero.");
+                    scanner.next(); // Pulisce l'input non valido
+                }
+            }
+
+            while (true) {
+                try {
+                    System.out.print("Seleziona la seconda carta (0, " + (deck.getSize() - 1) + "): ");
+                    secondIndex = scanner.nextInt();
+                    break; // Esce dal ciclo se l'input è valido
+                } catch (InputMismatchException e) {
+                    System.out.println("Input non valido! Inserisci un numero.");
+                    scanner.next(); // Pulisce l'input non valido
+                }
+            }
+
+            System.out.println();
+
+            if (firstIndex >= 0 && firstIndex < deck.getSize() && secondIndex >= 0 && secondIndex < deck.getSize() && firstIndex != secondIndex) {
                 Card firstCard = deck.drawCard(firstIndex);
                 Card secondCard = deck.drawCard(secondIndex);
 
-                System.out.println("Prima carta: " + firstCard.reveal());
-                System.out.println("Seconda carta: " + secondCard.reveal());
-
-                if (firstCard.getValue().equals(secondCard.getValue())) {
-                    System.out.println("Presa!");
-                    firstCard.setMatched(true);
-                    secondCard.setMatched(true);
+                // Verifica se le carte selezionate sono già abbinate
+                if (firstCard.isMatched() || secondCard.isMatched()) {
+                    System.out.println(underline("Una o entrambe le carte sono già abbinate. Scegli altre carte."));
                 } else {
-                    System.out.println("Sbagliato, riprova!");
+                    System.out.println("Prima carta: " + firstCard.reveal());
+                    System.out.println("Seconda carta: " + secondCard.reveal());
+                    System.out.println();
+
+                    if (firstCard.getValue().equals(secondCard.getValue())) {
+                        System.out.println(underline("Match trovato!"));
+                        firstCard.setMatched(true);
+                        secondCard.setMatched(true);
+                    } else {
+                        System.out.println(underline("Nessuna corrispondenza. Prova di nuovo."));
+                    }
                 }
 
                 checkGameEnded();
             } else {
-                System.out.println("Numeri inseriti non validi, riprova.");
+                System.out.println(underline("Numeri inseriti non validi. Prova di nuovo."));
             }
         }
-        System.out.println("Fine Gioco!");
+        System.out.println(underline("Fine Gioco!"));
         scanner.close();
     }
 
@@ -69,6 +95,10 @@ public class Game {
         gameEnded = true;
     }
 
+    public static String underline(String message) {
+        return message + "\n" +
+                "-".repeat(message.length());
+    }
     public static void main(String[] args) {
         Game game = new Game();
         game.play();
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("YDD Games");
+        System.out.println();
+
+        Game game = new Game();
+        game.play();
     }
 }
